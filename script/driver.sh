29d97dea85c90ea6eb9c3972b8a33a35a96f87c1
dipada <daniele.dipalma68@gmail.com>
2023-03-21 02:13:08 +0100

    fixed minimum frequency bug due to lscpu when smt disabled. Changed minimum frequency reads. Added option for save current configuration and disable smt/hyperthreading

--- script/driver.sh
+++ script/driver.sh
@@ -13,7 +13,8 @@ then
 fi 
 
 #readonly MAX_FREQ_MHZ=$(lscpu | grep -E '^CPU max MHz' | awk '{print $4}' | awk -F"." '{print $1}')
-readonly MIN_FREQ_MHZ=$(lscpu | grep -E '^CPU min MHz' | awk '{print $4}' | awk -F"." '{print $1}')
+#readonly MIN_FREQ_MHZ=$(lscpu | grep -E '^CPU min MHz' | awk '{print $4}' | awk -F"." '{print $1}')
+readonly MIN_FREQ_MHZ=$(cat /sys/devices/system/cpu/cpu0/cpufreq/cpuinfo_min_freq | awk '{print $1/1000}')
 readonly BASE_FREQ_MHZ=$(lscpu | grep 'Model name:' | awk '{print $8}' | cut -c1-4 | awk '{printf "%d", $1 * 1000}')
 
 readonly BASE_DIR=$(dirname $(pwd))
@@ -28,6 +29,10 @@ readonly RT_LOGS_DIR="$OUTPUT_DIR/rt-app-logs"
 # TODO signal Handler SIGINT
 
 cd ..
+
+# compile if needed rdmsr
+make
+
 # Create dirs if not exits
 if [ ! -d "$OUTPUT_DIR" ]; then
   echo "...creating $(basename $OUTPUT_DIR) directory"
@@ -65,6 +70,15 @@ if [ ! -d "$RT_LOGS_DIR" ]; then
 fi
 
 # Save current configuration
+echo "Saving current CPU configuration..."
+#echo $(lscpu | grep -E '^CPU min MHz') > $SETTING_DIR/stock_settings.txt
+echo "min freq $(cat /sys/devices/system/cpu/cpu0/cpufreq/cpuinfo_min_freq | awk '{print $1/1000}')" > $SETTING_DIR/stock_settings.txt
+#echo $(lscpu | grep -E '^CPU max MHz') >> $SETTING_DIR/stock_settings.txt
+echo "max freq $(cat /sys/devices/system/cpu/cpu0/cpufreq/cpuinfo_max_freq | awk '{print $1/1000}')" >> $SETTING_DIR/stock_settings.txt
+echo turbo $(cat /sys/devices/system/cpu/intel_pstate/no_turbo) >> $SETTING_DIR/stock_settings.txt
+echo hyperthreading $(cat /sys/devices/system/cpu/smt/active) >> $SETTING_DIR/stock_settings.txt
+
+
 
 echo "Frequency range ($MIN_FREQ_MHZ - $BASE_FREQ_MHZ MHz)"
 
@@ -76,35 +90,40 @@ cd script
 # disabling turbo
 ./control_turbo.sh
 
+# disabling hyper threading
+./control_ht.sh
+
+# execute tracing on rt-app on frequency range
+# every loop frequency will be increased by 100 mhz
 while [ $freq -le $BASE_FREQ_MHZ ]
 do
     echo "Actual $freq MHz - Target $BASE_FREQ_MHZ MHz"
 
-    sudo ./set_cpu_freq.sh $freq
+    ./set_cpu_freq.sh $freq
 
     (
       cd ..
-      make
-     #echo $(pwd)
+      #echo $(pwd)
       exec ./bin/read_msr INF > /dev/null
     )&
     read_msr_pid=$!
 
     (
       cd ..
-      echo $(pwd)
-      trace-cmd record -e sched_switch -f "prev_comm==\"rt-app\" || next_comm==\"rt-app\"" -e read_msr -f "msr==0x19c" -o $DAT_DIR/traceprov.dat rt-app $CONF_DIR/singleCAlter.json
-      exec trace-cmd report $DAT_DIR/traceprov.dat > $REP_DIR/$freq.txt 
+      #echo $(pwd)
+      trace-cmd record -q -e sched_switch -f "prev_comm==\"rt-app\" || next_comm==\"rt-app\"" -e read_msr -f "msr==0x19c" -o $DAT_DIR/$freq.dat rt-app $CONF_DIR/singleCAlter.json
+      exec trace-cmd report $DAT_DIR/$freq.dat > $REP_DIR/$freq.txt 
     )&
 
     trace_pid=$!
     
     wait $trace_pid
-    echo "trace finito"
     
     kill -SIGINT $read_msr_pid
     wait $read_msr_pid
     
+    echo -e "\n\n< Cooling the engines >\n\n"
+    sleep 3
 
     #sudo trace-cmd record -e sched_switch -f "prev_comm==\"rt-app\" || next_comm==\"rt-app\"" -e read_msr -f "msr==0x19c" rt-app singleCAlter.json && trace-cmd report > toParse.txt 
     
@@ -112,3 +131,22 @@ do
 
     freq=$(($freq + 100))
 done
+
+echo $(pwd)
+# restore stock configuration
+echo "Restoring stock CPU configuration..."
+#./control_turbo.sh $(cat $SETTING_DIR/stock_settings.txt | grep -E '^turbo' | awk '{print $2}')
+#./control_hyperthreading.sh $(cat $SETTING_DIR/stock_settings.txt | grep -E '^hyperthreading' | awk '{print $2}')
+echo $(cat $SETTING_DIR/stock_settings.txt | grep turbo | awk '{print $2}') > /sys/devices/system/cpu/intel_pstate/no_turbo
+if [ $(cat $SETTING_DIR/stock_settings.txt | grep hyperthreading | awk '{print $2}') == "1" ]; then
+  echo on | sudo tee /sys/devices/system/cpu/smt/control
+else
+  echo off | sudo tee /sys/devices/system/cpu/smt/control
+fi
+
+#S_MIN_FREQ=$(cat $SETTING_DIR/stock_settings.txt | grep -E '^CPU min MHz' | awk '{print $4}' | awk -F"." '{print $1}')
+S_MIN_FREQ=$(cat $SETTING_DIR/stock_settings.txt | grep 'min freq' | awk '{print $3}')
+#S_MAX_FREQ=$(cat $SETTING_DIR/stock_settings.txt | grep -E '^CPU max MHz' | awk '{print $4}' | awk -F"." '{print $1}')
+S_MAX_FREQ=$(cat $SETTING_DIR/stock_settings.txt | grep 'max freq' | awk '{print $3}')
+
+./set_cpu_freq.sh $S_MIN_FREQ $S_MAX_FREQ
