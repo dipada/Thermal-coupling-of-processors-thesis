0a747f17eef3303ad6028ac943e71b53a3c39f06
dipada <daniele.dipalma68@gmail.com>
2023-03-27 00:44:05 +0200

    added folders structure for multiple outputs

--- src/parser/preprocessing.py
+++ src/parser/preprocessing.py
@@ -5,13 +5,13 @@
 import sys
 import os
 import re
+import csv
 import datetime
 
 CURRENT_DIR = os.path.dirname(__file__)
 BASE_DIR = os.path.abspath(os.path.join(CURRENT_DIR, "../../"))
 REP_DIR = os.path.join(BASE_DIR, "output/reports/")
 CSV_DIR = os.path.join(BASE_DIR, "output/csv/")
-
 
 cpu_state = {}
 
@@ -20,29 +20,25 @@ def sched_switch_occurs(cpu, timestamp, desc):
     end_exec = r'.*==> swapper/\d+:0.*'
 
     if re.match(start_exec, desc): # something on current cpu starts executing
-        print("start")
         cpu_state[cpu] = {"start": timestamp}
     
     # print(f"sched_switch event at timestamp {timestamp} on cpu {cpu} with description: {desc}")
 
     elif re.match(end_exec, desc): # something on current cpu ends executing
         if cpu in cpu_state:
-            cpu_dir = os.path.join(CSV_DIR, cpu)
+
+            cpu_dir = os.path.join(in_file_dir, cpu)
             if not os.path.exists(cpu_dir):
                 os.mkdir(cpu_dir)
 
-            file_path = os.path.join(cpu_dir, f"executions_{cpu}.csv")
-            if not os.path.exists(file_path):
-                os.mknod(file_path)
-            
-            
+            file_path = os.path.join(cpu_dir, f"executions_{cpu}.csv") 
+            # appends to file the start timestamp by dictonary and the end timestamp by the current timestamp
             with open(file_path, "a") as f:
-            
-                f.write(cpu_state[cpu]["start"] + "," + timestamp + "\n")
+                writer = csv.writer(f)
+                writer.writerow([cpu_state[cpu]["start"], timestamp])
                 del cpu_state[cpu]
-                
-                
-        
+
+
 
 def read_msr_occurs(cpu, timestamp, desc):
     print(f"read_msr event at timestamp {timestamp} on cpu {cpu} with description: {desc}")
@@ -62,6 +58,15 @@ if not os.path.exists(file_path):
 # check if CSV directory exists and create it if not
 if not os.path.exists(CSV_DIR):
     os.mkdir(CSV_DIR)
+
+# Makes needed directories       
+date_dir = os.path.join(CSV_DIR, datetime.datetime.now().strftime("%d-%m-%Y-%H-%M-%S"))
+if not os.path.exists(date_dir):
+    os.mkdir(date_dir)
+
+in_file_dir = os.path.join(date_dir, file_name.split(".")[0])
+if not os.path.exists(in_file_dir):
+    os.mkdir(in_file_dir)
 
 # Regexs to match different parts of the line
 #proc_name = r'\s+(\S+)'  #
@@ -98,3 +103,5 @@ with open(file_path, "r") as f:
             elif event_type == "read_msr":
                 pass
                 #read_msr_occurs(cpu_num, time_stamp, description)
+
+print("Preprocessing >> Done!\nFiles are in " + os.path.relpath(date_dir) + " directory.")
