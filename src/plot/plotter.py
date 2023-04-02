aae278a2a7989098d3ab7c0c7c180bc19744a678
dipada <daniele.dipalma68@gmail.com>
2023-04-02 10:18:00 +0200

    added load_data function. Loading files according to project dir structure

--- src/plot/plotter.py
+++ src/plot/plotter.py
@@ -1,11 +1,49 @@
 import os
 import sys
+import csv
 
 
 CURRENT_DIR = os.path.dirname(__file__)
 BASE_DIR = os.path.abspath(os.path.join(CURRENT_DIR, "../../"))
 CSV_DIR = os.path.join(BASE_DIR, "output/csv/")
 PLOT_DIR = os.path.join(BASE_DIR, "output/plots/")
+
+start_times = {}
+end_times = {}
+temp_times = {}
+temp = {}
+
+def load_data(dir_path):
+    print("\nPlotting directory: " + dir_path)
+
+    # subdirs in dir_path, used to get number of cpus
+    num_subdir = len([f.path for f in os.scandir(dir_path) if f.is_dir()])
+
+    for i in range(num_subdir):
+
+        cpu = f"{i:03d}"
+        folder = os.path.join(dir_path, cpu)
+        print(folder)
+        executions_file = os.path.join(folder, f"executions_{cpu}.csv")
+        msr_readings_file = os.path.join(folder, f"msr_readings_{cpu}.csv")
+        
+        if os.path.isfile(executions_file):
+            start_times[cpu] = []
+            end_times[cpu] = []
+            with open(executions_file) as f:
+                reader = csv.reader(f)
+                for row in reader:
+                    start_times[cpu].append(float(row[0]))
+                    end_times[cpu].append(float(row[1]))
+
+        if os.path.isfile(msr_readings_file):
+            temp_times[cpu] = []
+            temp[cpu] = []
+            with open(msr_readings_file) as f:
+                reader = csv.reader(f)
+                for row in reader:
+                    temp_times[cpu].append(float(row[0]))
+                    temp[cpu].append(int(row[1]))
 
 print("Plotting...")
 print("Current directory: " + CURRENT_DIR)
@@ -30,9 +68,8 @@ if not os.path.exists(PLOT_DIR):
 if not os.path.exists(os.path.join(PLOT_DIR, files_dir)):
     os.mkdir(os.path.join(PLOT_DIR, files_dir))
 
-
-
-
-
-
-
+# loading and plotting data
+for subdir in os.listdir(files_path):
+    
+    subdir_path = os.path.join(files_path, subdir)
+    load_data(subdir_path)
