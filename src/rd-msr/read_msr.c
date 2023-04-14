9410963f0ae9129fb5e80f12f71f9e299214137e
dipada <daniele.dipalma68@gmail.com>
2023-04-14 23:48:03 +0200

    added single core msr reading mode

--- src/rd-msr/read_msr.c
+++ src/rd-msr/read_msr.c
@@ -9,13 +9,15 @@
 #define MSR_TERM_STATUS 0x19c        // cpus temp
 #define MSR_TEMPERATURE_TARGET 0x1a2 // for TjMax
 #define PATH_MSR_FILE "/dev/cpu/%d/msr"
-#define PRINT_USAGE   printf("Usage: \n(1) rdmsr for start program with default values      \
-                      \n(2) rdmsr ExecutionTime-(opt) readMsrEveryNanoSecs-(opt)            \
-                      \n(3) rdmsr INF-(unbounded execution) readMsrEveryNanoSecs-(opt)\n"); \
+#define PRINT_USAGE   printf("Usage: \n(1) rdmsr for start program with default values              \
+                      \n(2) rdmsr ExecutionTime-(opt) readMsrEveryNanoSecs-(opt)                    \
+                      \n(3) rdmsr INF-(unbounded execution) readMsrEveryNanoSecs-(opt)\n            \
+                      \n(4) rdmsr INF-(unbounded execution) readMsrEveryNanoSecs-(opt) cpu-(opt)"); \
                       exit(EXIT_FAILURE);
 #define UNBOUNDED "INF"
 #define DEFAULT_READ 100000000
 #define DEFAULT_DURATION 5
+#define READ_ALL_CPUS -1
 
 int main(int argc, char **argv){
   int fd;
@@ -36,6 +38,7 @@ int main(int argc, char **argv){
   read_nanosecs.tv_nsec = 0;
   
   char *end;
+  int rd_cpu = READ_ALL_CPUS;
 
   if(argv[1] != NULL){
     if(strcmp(argv[1],UNBOUNDED) == 0){
@@ -56,44 +59,69 @@ int main(int argc, char **argv){
   if(argc == 3 && argv[2] != NULL){
     end = NULL;
     read_nanosecs.tv_nsec = strtol(argv[2], &end, 10);
-    printf("2-end %c\n", *end);
     if(*end != '\0'){
       PRINT_USAGE
     }
   }else{ // default value, reads msr every 1 ms
     printf("Setting reads of MSRs every %dns\n", DEFAULT_READ);
-    read_nanosecs.tv_nsec = DEFAULT_READ;
-  }  
+    read_nanosecs.tv_nsec = 50000000;
+  }
+
+  if(argc == 4 && argv[3] != NULL){
+    end = NULL;
+    rd_cpu = strtol(argv[3], &end, 10);
+    if(*end != '\0'){
+      PRINT_USAGE
+    }
+  }
   
+  read_nanosecs.tv_nsec = 50000000;
   
   time_t start_time = time(NULL);
 
-  while (unbounded || (time(NULL) - start_time < duration_secs)){
+  if (rd_cpu != READ_ALL_CPUS){
+    // read msr of single CPU
+    sprintf(msr_path, PATH_MSR_FILE, rd_cpu);
+
+    if ((fd = open(msr_path, O_RDONLY)) == -1){
+      fprintf(stderr, "Error opening %s. Action not performed\n", msr_path);
+      exit(EXIT_FAILURE);
+    }
+
+    while (unbounded || (time(NULL) - start_time < duration_secs)){
+      if (pread(fd, &value, sizeof(value), MSR_TERM_STATUS) != sizeof(value)){
+        fprintf(stderr, "CPU[%2d] - error reading MSR\n", rd_cpu);
+      }
+      nanosleep(&read_nanosecs, NULL);
+    }
+
+    close(fd);
     
-    // Read MSRs of all CPUs
+  }else{
+    int fd_arr[n_cpus];
     for (register int i = 0; i < n_cpus; i++){
       sprintf(msr_path, PATH_MSR_FILE, i);
-      printf("[%2d] -- msr_path %s ", i, msr_path);
-
-      // read MSR of specific CPU
-      fd = open(msr_path, O_RDONLY);
-      if (fd < 0){
+      if ((fd_arr[i] = open(msr_path, O_RDONLY)) == -1){
         fprintf(stderr, "Error opening %s. Action not performed\n", msr_path);
-      }else{
-        if (pread(fd, &value, sizeof(value), MSR_TERM_STATUS) != sizeof(value)){
-          fprintf(stderr, "CPU[%2d] - error reading MSR\n", i);
-        }else{
-          printf("CPU[%2d] - 0x%lx\n", i, value >> 16 & 0x7F);
-        }      
+        exit(EXIT_FAILURE);
       }
-      close(fd);
-      
     }
-    fflush(stdout);
-    fflush(stderr);
-    nanosleep(&read_nanosecs, NULL);
-  }
 
+    // read MRS on all CPUs
+    while (unbounded || (time(NULL) - start_time < duration_secs)){
+      for (register int i = 0; i < n_cpus; i++){
+        if (pread(fd_arr[i], &value, sizeof(value), MSR_TERM_STATUS) != sizeof(value)){
+          fprintf(stderr, "CPU[%2d] - error reading MSR\n", rd_cpu);
+        }
+      }
+      nanosleep(&read_nanosecs, NULL);
+    }
+
+    for (register int i = 0; i < n_cpus; i++){
+      close(fd_arr[i]);
+    }
+
+  }
 
   return EXIT_SUCCESS;
 }
