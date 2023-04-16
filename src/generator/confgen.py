09ebd0c36acb21347582cdf72d4229ca61aa142f
dipada <daniele.dipalma68@gmail.com>
2023-04-16 17:23:48 +0200

    added thread obj collection

--- src/generator/confgen.py
+++ src/generator/confgen.py
@@ -23,22 +23,25 @@ def fill_global_obj(cpu, duration, calibration, default_policy):
 
     return global_obj
 
-thread_obj = {
-    "loop" : -1,
-    "cpus": [2],
-    "run" : 400000,
-    "sleep" : 400000
-}
+def fill_thread_obj(loop, cpu_affinity, run, sleep):
+    thread_obj = {
+        "loop" : loop,
+        "cpus": f"[{cpu_affinity}]",
+        "run" : run,
+        "sleep" : sleep
+    }
 
-final_json = {
-	"tasks" : {
-		"thread0" : thread_obj
-	},
-	"global" : 9
-}
+    return thread_obj
 
-for i in range(1, 2):
-    final_json["tasks"][f"thread{i}"] = thread_obj
+#final_json = {
+#	"tasks" : {
+#		"thread0" : thread_obj
+#	},
+#	"global" : 9
+#}
+
+#for i in range(1, 2):
+#    final_json["tasks"][f"thread{i}"] = thread_obj
 
 
 #with open(f'{RES_DIR}/test.json', 'w') as outfile:
@@ -55,16 +58,70 @@ def make_json(duration):
     for i in range(1, 2):
         final_json["tasks"][f"thread{i}"] = thread_obj
 
-
-def collect_data(): 
+def global_obj_data(user_data):
+    
     # Global object default values
     default_duration = 10
     default_calibration = "CPU0"
     default_policy = "SCHED_OTHER"
 
+    duration = input("Enter the duration of the experiment, default is 10s: ")
+
+    if duration == "" or not duration.isdigit():
+        print(f"Invalid duration, using default value ({default_duration}))")
+        duration = default_duration
+    
+    calibration = input("Enter the calibration value, default is \"CPU0\":")
+
+    if calibration == "" or calibration == -1 or (not calibration.isdigit() and not re.compile(r"CPU[0-9]+").match(calibration)):
+        print(f"Invalid calibration value, using default value ({default_calibration})")
+        calibration = default_calibration
+
+    sched_policy = input("Enter the default policy, default is \"SCHED_OTHER\": ")
+
+    if sched_policy != "SCHED_OTHER" and sched_policy != "SCHED_FIFO" and sched_policy != "SCHED_RR" and sched_policy != "SCHED_DEADLINE":
+        print(f"Invalid policy, using default value ({default_policy})")
+        sched_policy = default_policy
+
+    user_data["global_duration"] = duration
+    user_data["calibration"] = calibration
+    user_data["global_default_policy"] = sched_policy
+
+def thread_obj_data(user_data):
     # Thread object default values
     default_thread_loop = -1
+    default_cpu_affinity = 0
+    default_thread_run = 500000
+    default_thread_sleep = 500000
 
+    loop = input(f"Enter the loop value for thread, default is {default_thread_loop}: ")
+    if loop == "" or not loop.isdigit():
+        print(f"Invalid loop value, using default value {default_thread_loop}")
+        loop = default_thread_loop
+
+    cpu_affinity = input(f"Enter the CPU affinity for thread, default is {default_cpu_affinity}: ")
+    if cpu_affinity == "" or not cpu_affinity.isdigit():
+        print(f"Invalid CPU affinity value, using default value {default_cpu_affinity}")
+        cpu_affinity = default_cpu_affinity
+
+    run = input(f"Enter the run value for thread, default is {default_thread_run} usec: ")
+    if run == "" or not run.isdigit():
+        print(f"Invalid run value, using default value {default_thread_run}")
+        run = default_thread_run
+    
+    sleep = input(f"Enter the sleep value for thread, default is {default_thread_sleep} usec: ")
+    if sleep == "" or not sleep.isdigit():
+        print(f"Invalid sleep value, using default value {default_thread_sleep}")
+        sleep = default_thread_sleep
+
+    user_data["thread_loop"] = loop
+    user_data["thread_cpu_affinity"] = cpu_affinity
+    user_data["thread_run"] = run
+    user_data["thread_sleep"] = sleep
+
+
+
+def collect_data(): 
 
     user_data = {}
 
@@ -84,36 +141,14 @@ def collect_data():
         exit(1)
 
     # Global object values
-    duration = input("Enter the duration of the experiment, default is 10s: ")
-
-    if duration == "" or not duration.isdigit():
-        print(f"Invalid duration, using default value ({default_duration}))")
-        duration = default_duration
-    
-    calibration = input("Enter the calibration value, default is \"CPU0\":")
-
-    if calibration == "" or calibration == -1 or (not calibration.isdigit() and not re.compile(r"CPU[0-9]+").match(calibration)):
-        print(f"Invalid calibration value, using default value ({default_calibration})")
-        calibration = default_calibration
-
-    sched_policy = input("Enter the default policy, default is \"SCHED_OTHER\": ")
-
-    if sched_policy != "SCHED_OTHER" and sched_policy != "SCHED_FIFO" and sched_policy != "SCHED_RR" and sched_policy != "SCHED_DEADLINE":
-        print(f"Invalid policy, using default value ({default_policy})")
-        sched_policy = default_policy
+    global_obj_data(user_data)
 
     # thread object values
-    loop = input("Enter the loop value for thread, default is -1: ")
-    if loop == "" or not loop.isdigit():
-        print(f"Invalid loop value, using default value {default_thread_loop}")
-        loop = default_thread_loop
-    
+    thread_obj_data(user_data)
 
     user_data["single_json"] = single_json_generation
     user_data["cpu"] = cpu
-    user_data["global_duration"] = duration
-    user_data["calibration"] = calibration
-    user_data["global_default_policy"] = sched_policy
+    
 
     return user_data
 
@@ -127,12 +162,15 @@ if __name__ == "__main__":
     if(user_values["single_json"]): # Single json file generation
         json_name = f"cpu{user_values['cpu']}"
         global_obj = fill_global_obj(user_values["cpu"], user_values["global_duration"], user_values["calibration"], user_values["global_default_policy"] )
+        thread_obj = fill_thread_obj(user_values["thread_loop"], user_values["thread_cpu_affinity"], user_values["thread_run"], user_values["thread_sleep"])
         json_name = json_name + f"-{global_obj['duration']}s.json"
         print(json_name)
         print("\n\n")
         print(json.dumps(global_obj, indent=4))
+        print("\n\n")
+        print(json.dumps(thread_obj, indent=4))
     else:
-        for i in range(1, int(user_values["cpu"])):
+        for i in range(0, int(user_values["cpu"])):
             json_name = f"cpu{i}"
             global_obj = fill_global_obj(i, user_values["global_duration"], user_values["calibration"], user_values["global_default_policy"])
             json_name = json_name + f"-{global_obj['duration']}s.json"
