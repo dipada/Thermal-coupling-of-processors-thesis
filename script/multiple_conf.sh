fde74badb518ddecc29ea65a89818c7273ec7bea
dipada <daniele.dipalma68@gmail.com>
2023-04-16 22:13:49 +0200

    added new configuration and removed old

--- script/multiple_conf.sh
+++ script/multiple_conf.sh
@@ -4,17 +4,16 @@ ROOT_UID=0     # Only users with $UID 0 have root privileges.
 E_NOTROOT=67   # Non-root exit error.
 
 
+readonly BASE_DIR=$(dirname $(pwd))
+readonly RES_DIR="$BASE_DIR/resources"
+readonly CONF_DIR="$RES_DIR/configuration"
+
 if [ "$UID" -ne "$ROOT_UID" ]
 then
   echo "Must be root to run this script."
   exit $E_NOTROOT
 fi
 
-./driver.sh "-rt" "singleCDeadLine.json"
-echo "Cooling..."
-sleep 30
-./driver.sh "-rt" "fastAlternDL.json"
-echo "Cooling..."
-sleep 30
-./driver.sh "-rt" "alterHalfDL.json"
-
+for file in $CONF_DIR/singleCore/*.json; do
+    echo $file
+done
